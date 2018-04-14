60afbe8e4e125652fb5dc4af2f5d41b18c3b036d
pushpendu purkait <pushpendupurkait@gmail.com>
2018-04-14 23:09:00 +0530

    added actor scheduler

--- src/main/scala/MainParking.scala
+++ src/main/scala/MainParking.scala
@@ -1,13 +1,17 @@
 import akka.actor.{ActorSystem, Props}
 import com.pushp.ParkingGateOne
 import scala.io.StdIn
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object MainParking extends App{
   val actorSystem = ActorSystem("parking-lot")
   val parkingGateOneActor = actorSystem.actorOf(Props[ParkingGateOne],name = "Parking-gate-1")
 
-  parkingGateOneActor ! "Car entered"
-  parkingGateOneActor ! "Car"
+/*  parkingGateOneActor ! "Car entered"
+  parkingGateOneActor ! "Car"*/
+
+  actorSystem.scheduler.schedule(2 seconds, 5 seconds, parkingGateOneActor, "Scheduled Msg")
 
   StdIn.readLine()
   actorSystem.terminate()
